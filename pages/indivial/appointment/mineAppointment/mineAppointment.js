63445156b983e47a6691177c7c5b23e7bfda7353
PorUnaCabeza <makpia@163.com>
2018-12-09 22:57:19 +0800

    appointment

---
+++ pages/indivial/appointment/mineAppointment/mineAppointment.js
@@ -0,0 +1,82 @@
+// pages/indivial/appointment/mineAppointment/mineAppointment.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [
+      {
+        name: '王二小',
+        tel: '13656873290',
+        address: '江苏省南京市秦淮区来凤街菱角市22号'
+      },
+      {
+        name: '王二小',
+        tel: '13656873290',
+        address: '江苏省南京市秦淮区来凤街菱角市22号'
+      },
+      {
+        name: '王二小',
+        tel: '13656873290',
+        address: '江苏省南京市秦淮区来凤街菱角市22号'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
